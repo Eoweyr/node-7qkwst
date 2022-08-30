83d7d8ab854ce0a596fe2d227426af50c6cc28ea
Eoweyr <salvagr@gmail.com>
2022-08-30 15:02:07 +0200

    2 files modified

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 require('dotenv');
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const router = require('./servidor/rutas/rutasPublicas');
 
 const puerto = process.env.PUERTO || 3002;
 const app = express();
-const routerPublico = require('./servidor/rutas/rutasPublicas');
+
 
 app.use('dotenv');
 app.use(expressLayouts);
 
 app.set('layout', 'layouts/publica');
 
-app.use('/', routerPublico);
+app.use('/', router);
 
 app.listen(puerto, () => {
   console.log(`Servidor conectado en el puerto: ${puerto}`);
--- servidor/rutas/rutasPublicas.js
+++ servidor/rutas/rutasPublicas.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const routerPublico = express.Router();
+const router = express.Router();
 
 
-routerPublico.get('/', (req, res) => {
+router.get('/', (req, res) => {
   res.send('hola');
 })
 
 
-module.exports = routerPublico;
+module.exports = router;
